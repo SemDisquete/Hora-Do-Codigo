00f1d7c9cef59c899e83301d93cab9016250d53e
Helder J <helderjfl@gmail.com>
2015-06-02 22:29:14 -0300

    Passo do template

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by HelderJefferson on 02/06/2015.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
