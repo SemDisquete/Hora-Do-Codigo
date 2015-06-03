4ded4bd1b77933aa8fda33c509780c6bb6314ab6
Richard Frank <richardfrk@gmail.com>
2015-06-03 15:22:09 -0300

    Initial Commit

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-=======
-/**
- * Created by HelderJefferson on 02/06/2015.
- */
->>>>>>> fac7314b1c120e14d6b314b414f5db7c6bac3719
 Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
     }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> fac7314b1c120e14d6b314b414f5db7c6bac3719
+
--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-/**
- * Created by HelderJefferson on 02/06/2015.
- */
->>>>>>> fac7314b1c120e14d6b314b414f5db7c6bac3719
 var postsData = [
     {
         title: 'Introducing Telescope',
@@ -21,13 +15,9 @@ var postsData = [
         url: 'http://themeteorbook.com'
     }
 ];
-<<<<<<< HEAD
 
 Template.postList.helpers({
     posts: postsData
 });
-=======
-Template.postsList.helpers({
-    posts: postsData
-});
->>>>>>> fac7314b1c120e14d6b314b414f5db7c6bac3719
+
+
